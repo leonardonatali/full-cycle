29e8a7a7e16f667293c83bc323f4d95a162670ef
Leonardo Natali <leonardo.natali@hotmail.com>
2021-07-24 22:10:59 -0300

    feat: adds repository definitions, user entity and attached service

--- grpc/pkg/services/user_service.go
+++ grpc/pkg/services/user_service.go
@@ -3,13 +3,35 @@ package services
 import (
 	"context"
 
+	"github.com/leonardonatali/full-cycle/grpc/pkg/entities"
 	"github.com/leonardonatali/full-cycle/grpc/pkg/protobuf/users"
+	"github.com/leonardonatali/full-cycle/grpc/pkg/repository"
 )
 
 type UserService struct {
 	users.UnimplementedUserServiceServer
+	usersRepo repository.UsersRepository
+}
+
+func NewUsersService(usersRepo repository.UsersRepository) *UserService {
+	return &UserService{
+		usersRepo: usersRepo,
+	}
 }
 
 func (s UserService) Add(ctx context.Context, req *users.User) (user *users.User, err error) {
-	return &users.User{}, nil
+	res, err := s.usersRepo.Add(&entities.User{
+		Name:  req.Name,
+		Email: req.Email,
+	})
+
+	if err != nil {
+		return
+	}
+
+	return &users.User{
+		Id:    res.ID,
+		Name:  res.Name,
+		Email: res.Email,
+	}, nil
 }
